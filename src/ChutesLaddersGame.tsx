1fbad8acedca4be51717fda78b99c7089ac32427
Shandon <shandonwayne@gmail.com>
2025-08-19 08:24:40 -0400

    Add iron background to inactive dice

--- src/ChutesLaddersGame.tsx
+++ src/ChutesLaddersGame.tsx
@@ -168,7 +168,9 @@ const ChutesLaddersGame: React.FC = () => {
           style={{ 
             width: '95%', 
             height: '95%',
-            filter: 'brightness(0) saturate(100%)' // Makes SVG black
+            filter: 'brightness(0) saturate(100%)', // Makes SVG black
+            backgroundColor: '#3A342E', // Iron background
+            borderRadius: 'var(--border-radius)'
           }}
         />
       );
